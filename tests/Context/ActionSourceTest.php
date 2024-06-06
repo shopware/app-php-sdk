4b4550344530eb3bcd3b721a59040479376e1d75
Lennart Tinkloh <l.tinkloh@shopware.com>
2024-06-06 16:51:18 +0200

    NEXT-36521 - in app purchases

--- tests/Context/ActionSourceTest.php
+++ tests/Context/ActionSourceTest.php
@@ -11,7 +11,7 @@ use Shopware\App\SDK\Context\ActionSource;
 #[CoversClass(ActionSource::class)]
 class ActionSourceTest extends TestCase
 {
-    public function testConstruct(): void
+    public function testConstructDefaults(): void
     {
         $url = 'https://example.com';
         $version = '1.0.0';
@@ -20,5 +20,25 @@ class ActionSourceTest extends TestCase
 
         static::assertSame($url, $source->url);
         static::assertSame($version, $source->appVersion);
+        static::assertSame([], $source->inAppPurchases);
+
+        static::assertFalse($source->hasInAppPurchase('purchase1'));
+    }
+
+    public function testConstruct(): void
+    {
+        $url = 'https://example.com';
+        $version = '1.0.0';
+        $inAppPurchases = ['purchase1', 'purchase2'];
+
+        $source = new ActionSource($url, $version, $inAppPurchases);
+
+        static::assertSame($url, $source->url);
+        static::assertSame($version, $source->appVersion);
+        static::assertSame($inAppPurchases, $source->inAppPurchases);
+
+        static::assertTrue($source->hasInAppPurchase('purchase1'));
+        static::assertTrue($source->hasInAppPurchase('purchase2'));
+        static::assertFalse($source->hasInAppPurchase('purchase3'));
     }
 }
