bf2d87c8c900bb6bec9db3c29f1d29b4671dce50
Lennart Tinkloh <l.tinkloh@shopware.com>
2024-06-06 16:51:18 +0200

    NEXT-36521 - in app purchases

--- src/Context/Module/ModuleAction.php
+++ src/Context/Module/ModuleAction.php
@@ -11,12 +11,14 @@ class ModuleAction
     /**
      * @param string $contentLanguage - The language of the Shopware content as UUID
      * @param string $userLanguage - The language of the Shopware user as ISO (en-GB)
+     * @param string[] $inAppPurchases - The active in-app-purchases
      */
     public function __construct(
         public readonly ShopInterface $shop,
         public readonly string $shopwareVersion,
         public readonly string $contentLanguage,
         public readonly string $userLanguage,
+        public readonly array $inAppPurchases = [],
     ) {
     }
 }
