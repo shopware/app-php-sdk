c1c5e791692afbc5209b01a8c321f85f911c9685
Michel Bade <m.bade@shopware.com>
2026-03-29 19:17:36 +0200

    feat: getters for exception parameters

--- src/Exception/MissingClaimException.php
+++ src/Exception/MissingClaimException.php
@@ -6,8 +6,14 @@ namespace Shopware\App\SDK\Exception;
 
 class MissingClaimException extends \RuntimeException
 {
-    public function __construct(string $claimName)
-    {
+    public function __construct(
+        private string $claimName,
+    ) {
         parent::__construct(\sprintf('Missing claim "%s", did you forgot to add permissions in your app to this?', $claimName));
+    }
+
+    public function getClaimName(): string
+    {
+        return $this->claimName;
     }
 }
--- src/Exception/ShopNotFoundException.php
+++ src/Exception/ShopNotFoundException.php
@@ -6,8 +6,15 @@ namespace Shopware\App\SDK\Exception;
 
 class ShopNotFoundException extends \RuntimeException
 {
-    public function __construct(string $shopId, ?\Throwable $previous = null)
-    {
+    public function __construct(
+        private readonly string $shopId,
+        ?\Throwable $previous = null,
+    ) {
         parent::__construct(sprintf('Shop with id "%s" not found', $shopId), 0, $previous);
+    }
+
+    public function getShopId(): string
+    {
+        return $this->shopId;
     }
 }
